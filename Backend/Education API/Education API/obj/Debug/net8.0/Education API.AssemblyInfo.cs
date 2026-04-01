b6b5902c2bbb0296a9148d4dfe036c4c7ea133c2
yusif-tagiyev <7dt287t@code.edu.az>
2026-04-02 00:26:20 +0400

    update site main

--- Backend/Education API/Education API/obj/Debug/net8.0/Education API.AssemblyInfo.cs
+++ Backend/Education API/Education API/obj/Debug/net8.0/Education API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Education API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88bad3a7085c3144aac32a17cb72fab699487e5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6db4e0d31a6df91297195cc43d60260e4d8f76bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Education API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Education API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/assets/js/navbar.js
+++ frontend/assets/js/navbar.js
@@ -5,23 +5,30 @@ const nav = document.querySelector("nav");
 const mainDivForNAvbar = document.querySelector(".mainDivForNAvbar");
 const navLogInPage = document.querySelector(".navLogInPage");
 const logInExitBtn = document.querySelector(".logInExitBtn");
-console.log(logInExitBtn);
-console.log(nav);
-console.log(mainDivForNAvbar);
-console.log(logInBtn);
-console.log(navLogInPage);
+// console.log(logInExitBtn);
+// console.log(nav);
+// console.log(mainDivForNAvbar);
+// console.log(logInBtn);
+// console.log(navLogInPage);
 
 logInBtn.addEventListener("click", function () {
   console.log(nav);
-  nav.style.zIndex = "-1";
-  mainDivForNAvbar.style.opacity = "0";
-  navLogInPage.classList.add("hiddenDiv");
+  // nav.style.zIndex = "-1";
+  nav.style.opacity = "0";
+  nav.style.display = "none";
+  navLogInPage.classList.toggle("hiddenDiv");
   logInExitBtn.style.display = "flex";
 });
 
 logInExitBtn.addEventListener("click", function () {
-  navLogInPage.classList.remove("hiddenDiv");
-  nav.style.zIndex = "10";
-  mainDivForNAvbar.style.opacity = "1";
+  console.log("salam men islemirem");
+  logInExitBtn.style.zIndex = "9999";
+  nav.style.display = "block";
+  nav.style.opacity = "10";
+
   logInExitBtn.style.display = "none";
+  navLogInPage.classList.remove("hiddenDiv");
 });
+
+// // qeydiyyyat bolmesinin effektleri
+// const qeydiyyatBtn = document.querySelector("#qeydiyyatBtn");
